e54aaba17b80def0ba8eb9aad26c883ccaa2f4a1
Victoria Puls <vicke.puls@hotmail.com>
2020-07-28 16:07:42 -0300

    first-commit

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function ButtonLink(props) {
+    //props = { className: "o que for passado", href="/" }
+
+    return (
+    <a href={props.href} className={props.className} >
+        {props.children}
+    </a>
+    );
+}
+
+export default ButtonLink;
